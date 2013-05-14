6efdd72438fe77b3c2e8ff84647f07345522ac22
Ruben De Smet <ruben.de.smet@telenet.be>
2013-05-14 18:36:55 +0200

    Basic implementation

--- libsqlite.hpp
+++ libsqlite.hpp
@@ -1,5 +1,173 @@
 #include <sqlite3.h>
+#include <string>
+#include <vector>
+#include <exception>
+#include <memory>
+
+#ifndef SQLITEPP
+#define SQLITEPP
 
 namespace sqlite
 {
+    class sqlite;
+    class statement;
+    
+    class exception : public std::exception
+    {
+        friend statement;
+        friend sqlite;
+    public:
+        exception(std::string msg)
+        {
+            this->_msg = msg;
+        }
+        std::string what()
+        {
+            return "Sqlite error: " + this->_msg;
+        }
+    private:
+        void _set_errorno(int err)
+        {
+        }
+        std::string _msg;
+    };
+
+    class statement
+    {
+        friend sqlite;
+        friend exception;
+    public:
+        void set_sql(std::string sql)
+        {
+            if(this->_prepared)
+            {
+                exception e("Can not set sql on prepared query.");
+                throw e;
+            }
+            else
+            {
+                this->_sql = sql;
+            }
+        }
+        void prepare()
+        {
+            this->_prepared = true;
+            const char* tail;
+            int rc = sqlite3_prepare_v2(this->_db, 
+                    this->_sql.c_str(), 
+                    this->_sql.length(), 
+                    &this->_s, 
+                    &tail);
+            if(rc != SQLITE_OK)
+            {
+                exception e("Could not prepare sql.");
+                e._set_errorno(rc);
+                throw e;
+            }
+            this->_tail = std::string(tail);
+        }
+
+        bool step()
+        {
+            if(!this->_valid)
+            {
+                exception e("Trying to step an invalid statement.");
+            }
+
+            int rc = sqlite3_step(this->_s);
+            if(rc == SQLITE_DONE)
+            {
+                this->_valid = false;
+                return false;
+            }
+            if(rc == SQLITE_ROW)
+            {
+                this->_has_row = true;
+                return true;
+            }
+            // Ok, this means error if we get here
+            exception e("Sqlite had an error: " + std::string(sqlite3_errmsg(this->_db)));
+            return false;
+        }
+        void reset()
+        {
+            int rc = sqlite3_reset(this->_s);
+            if(rc != SQLITE_OK)
+            {
+                exception e("Could not reset the virtual machine.");
+                throw e;
+            }
+            this->_valid = true;
+            this->_has_row = false;
+            this->_prepared = false;
+        }
+
+        void exec()
+        {
+            this->prepare();
+            this->step();
+        }
+
+        virtual ~statement()
+        {
+            sqlite3_finalize(this->_s);
+        }
+    private:
+        statement(sqlite3* db)
+        {
+            this->_db = db;
+            this->_prepared = false;
+            this->_valid = true;
+            this->_has_row = false;
+        }
+        statement(sqlite3* db, std::string sql)
+        {
+            this->_db = db;
+            this->set_sql(sql);
+            this->_prepared = false;
+            this->_valid = true;
+            this->_has_row = false;
+        }
+        sqlite3* _db;
+        bool _prepared, _valid, _has_row;
+        std::string _sql;
+        std::string _tail;
+        sqlite3_stmt* _s;
+    };
+
+    class sqlite
+    {
+        friend statement;
+    public:
+        sqlite(std::string filename) throw (exception)
+        {
+            this->_filename = filename;
+            int rc = sqlite3_open(filename.c_str(), &this->_db);
+            if(rc != SQLITE_OK)
+            {
+                exception e("Could not open '" + filename + "'");
+                throw e;
+            }
+        }
+        std::shared_ptr<statement> get_statement()
+        {
+            std::shared_ptr<statement> st(new statement(this->_db));
+            return st;
+        }
+        std::shared_ptr<statement> get_statement(std::string sql)
+        {
+            std::shared_ptr<statement> st(new statement(this->_db, sql));
+            return st;
+        }
+        virtual ~sqlite()
+        {
+            sqlite3_close(this->_db);
+        }
+    private:
+        std::string _filename;
+
+        sqlite3* _db;
+    };
 }
+
+#endif
--- tests.cpp
+++ tests.cpp
@@ -1,6 +1,37 @@
+#include <iostream>
+
 #include "libsqlite.hpp"
 
 int main()
 {
+    sqlite::sqlite db(":memory:");  // Open an in-memory database
+                                    // Will auto close on destruction
+    std::shared_ptr<sqlite::statement> s = db.get_statement();
+    s->set_sql("CREATE TABLE test(id INTEGER PRIMARY KEY, text TEXT)");
+    s->exec();
+
+    // When shared_ptr is assigned another value, it deletes its contents.
+    // So s = db.get_statement(); Would do almost the same, except it would
+    // clear its bindings, which are NOT done now! As we don't have bindings,
+    // we don't care right now.
+    s->reset();
+
+    // INSERT example without bind.
+    s->set_sql("INSERT INTO test(text) VALUES('test')");
+    s->exec();
+
+    std::cout << "Inserted into test without binding values." << std::endl;
+
+    // Reset the statement again.
+    s->reset();
+
+    // SELECT example.
+    s->set_sql("SELECT id, text FROM test");
+    s->prepare();
+    while(s->step())
+    {
+        std::cout << "Row: " << std::endl;
+    }
+
     return 0;
 }
